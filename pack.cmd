5a93cbf9398f161491f6ef014e0b98b0fb00f214
Julian Voigt <baratock@hotmail.de>
2019-01-22 22:55:34 +0100

    zip from pack command

--- pack.cmd
+++ pack.cmd
@@ -1,6 +1,11 @@
+echo "copy files"
 xcopy ORS.c4d\* tmp\ORS.c4d /s /Y
 xcopy ORS.c4f\* tmp\ORS.c4f /s /Y
 
+echo "pack files"
 "../Clonk Rage/c4group" tmp\ORS.c4d -p
 "../Clonk Rage/c4group" tmp\ORS.c4f -p
+
+echo "zip it"
+Compress-Archive tmp\ORS.c4d tmp\ORS.c4f ORS.zip
 pause
